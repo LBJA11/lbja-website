876223a1b41fbd372a2278d4c81b316dec54e11c
LBJA11 <samchadha30@gmail.com>
2025-08-05 18:01:22 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('presale-form').addEventListener('submit', function(e){
+    e.preventDefault();
+    alert('You have successfully joined the presale! We will contact you soon.');
+});
