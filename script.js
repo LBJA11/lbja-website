fb6c8674a886e835be41f6fca3ba70a4f65816a8
LBJA11 <samchadha30@gmail.com>
2025-08-05 18:13:11 +0530

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('presale-form').addEventListener('submit', function(e){
-    e.preventDefault();
-    alert('You have successfully joined the presale! We will contact you soon.');
-});
